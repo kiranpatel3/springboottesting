5c971ec40fd102c7b33af896e560a711827e83b3
Kiran Patel <kiranpatel5394@gmail.com>
2018-12-04 13:52:06 -0500

    vid20

--- src/main/java/com/kiran/springboottest/information/InformationController.java
+++ src/main/java/com/kiran/springboottest/information/InformationController.java
@@ -3,8 +3,7 @@ package com.kiran.springboottest.information;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class InformationController {
@@ -15,5 +14,24 @@ public class InformationController {
     @RequestMapping("/information")
     public List<Information> getAllInformation(){
         return informationService.getAllInformation();
+    }
+    @RequestMapping("/information/{id}")
+    public Information getInformation(@PathVariable String id){
+        return informationService.getInformation(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/information")
+    public void addInformation(@RequestBody Information information){
+        informationService.addInformation(information);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/information/{id}")
+    public void updateInformation(@RequestBody Information information, @PathVariable String id){
+        informationService.addInformation(id, information);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/information/{id}")
+    public void deleteInformation(@PathVariable String id){
+        informationService.deleteInformation(id);
     }
 }
--- src/main/java/com/kiran/springboottest/information/InformationService.java
+++ src/main/java/com/kiran/springboottest/information/InformationService.java
@@ -8,14 +8,41 @@ import java.util.*;
 public class InformationService {
 
 
-    List<Information> info = Arrays.asList(
+    List<Information> info = new ArrayList<>(Arrays.asList(
                 new Information("a0021", "Mark", "Sloan"),
                 new Information("a0022", "Addison", "Montgomry"),
                 new Information("a0023", "Derek", "Shepherd"),
                 new Information("a0024", "Meredith", "Grey")
-                );
+                ));
 
     public List<Information> getAllInformation(){
         return info;
+    }
+    public Information getInformation(String id){
+        for (Information information : info) {
+            if(information.getId().equals(id)){
+                return information;
+            }
+        }
+        return null;
+//        return info.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addInformation(Information information) {
+        info.add(information);
+    }
+
+    public void addInformation(String id, Information information) {
+        for(int i = 0; i< info.size(); i++){
+            Information in = info.get(i);
+            if(in.getId().equals(id)){
+                info.set(i, information);
+                return;
+            }
+        }
+    }
+
+    public void deleteInformation(String id) {
+        info.removeIf(t -> t.getId().equals(id));
     }
 }
